4812808cd9d47329e25a8e1901652b30e2499714
Meraj <meraj.enigma@gmail.com>
2023-03-15 15:53:35 -0400

    gen_stage with one producer and two consumers

---
+++ scrapper/lib/page_producer.ex
@@ -0,0 +1,33 @@
+defmodule PageProducer do
+  @moduledoc false
+
+  use GenStage
+  require Logger
+
+  def scrape_pages(pages) when is_list(pages) do
+    GenStage.cast(__MODULE__, {:pages, pages})
+  end
+
+  def start_link(_args) do
+    initial_state = []
+    GenStage.start_link(__MODULE__, initial_state, name: __MODULE__)
+  end
+
+  @impl GenStage
+  def init(initial_state) do
+    Logger.info("PageProducer init")
+    {:producer, initial_state}
+  end
+
+  @impl GenStage
+  def handle_demand(demand, state) do
+    Logger.info("PageProducer received demand for #{demand} pages")
+    events = []
+    {:noreply, events, state}
+  end
+
+  @impl GenStage
+  def handle_cast({:pages, pages}, state) do
+    {:noreply, pages, state}
+  end
+end
---
+++ scrapper/lib/scrapper.ex
@@ -0,0 +1,10 @@
+defmodule Scrapper do
+  @moduledoc false
+
+  def work do
+    1..5
+    |> Enum.random()
+    |> :timer.seconds()
+    |> Process.sleep()
+  end
+end
