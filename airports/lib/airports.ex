178d0ec4134f97b8a1153c3a995883ad8f8c97a1
Meraj <meraj.enigma@gmail.com>
2023-03-17 16:39:54 -0400

    use window and trigger

--- airports/lib/airports.ex
+++ airports/lib/airports.ex
@@ -8,6 +8,10 @@ defmodule Airports do
   def open_airports do
     airports_csv()
     |> File.stream!()
+    #    |> Stream.map(fn event ->
+    #      Process.sleep(Enum.random([0, 0, 0, 1]))
+    #      event
+    #    end)
     |> Flow.from_enumerable(stages: 1)
     |> Flow.map(fn row ->
       [row] = CSV.parse_string(row, skip_headers: false)
@@ -20,11 +24,26 @@ defmodule Airports do
       }
     end)
     |> Flow.reject(&(&1.type == "closed"))
-    |> Flow.partition(key: {:key, :country})
+    |> Flow.partition(window: window(), key: {:key, :country})
     |> Flow.group_by(& &1.country)
-    |> Flow.map(fn {country, data} -> {country, Enum.count(data)} end)
-    |> Flow.take_sort(10, fn {_, a}, {_, b} -> a > b end)
+    |> Flow.on_trigger(fn acc, _partition_info, {_type, _id, trigger} = _window_info ->
+      events =
+        acc
+        |> Enum.map(fn {country, data} -> {country, Enum.count(data)} end)
+      # |> IO.inspect(label: inspect(self()))
+
+      case trigger do
+        :done ->
+          {events, acc}
+
+        {:every, 1000} ->
+          {[], acc}
+      end
+    end)
     |> Enum.to_list()
-    |> List.flatten()
+    |> Enum.sort(fn {_, a}, {_, b} -> a > b end)
+    |> Enum.take(10)
   end
+
+  defp window, do: Flow.Window.trigger_every(Flow.Window.global(), 1000)
 end
