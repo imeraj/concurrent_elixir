2c4c6e6d0f7843b1a6ec91682028c96cb17a2426
Meraj <meraj.enigma@gmail.com>
2023-03-18 08:47:16 -0400

    use handle_message and prepare_messages

--- tickets/lib/bookings_pipeline.ex
+++ tickets/lib/bookings_pipeline.ex
@@ -22,4 +22,36 @@ defmodule BookingsPipeline do
 
     Broadway.start_link(__MODULE__, options)
   end
+
+  # callbacks
+  @impl Broadway
+  def handle_message(_processor, message, _context) do
+    %{data: %{event: event, user: user}} = message
+
+    Tickets.create_ticket(user, event)
+    Tickets.send_email(user)
+
+    IO.inspect(message, label: "Message")
+  end
+
+  @impl Broadway
+  def prepare_messages(messages, _context) do
+    messages =
+      Enum.map(messages, fn message ->
+        Broadway.Message.update_data(message, fn data ->
+          [event, user_id] = String.split(data, ",")
+          %{event: event, user_id: user_id}
+        end)
+      end)
+
+    users = Tickets.users_by_ids(Enum.map(messages, & &1.data.user_id))
+
+    # put users in messages
+    Enum.map(messages, fn message ->
+      Broadway.Message.update_data(message, fn data ->
+        user = Enum.find(users, &(data.user_id == &1.id))
+        Map.put(data, :user, user)
+      end)
+    end)
+  end
 end
--- tickets/lib/tickets.ex
+++ tickets/lib/tickets.ex
@@ -1,18 +1,27 @@
 defmodule Tickets do
-  @moduledoc """
-  Documentation for `Tickets`.
-  """
+  @moduledoc false
 
-  @doc """
-  Hello world.
+  # helpers
+  def tickets_available?(_event) do
+    Process.sleep(Enum.random(100..200))
+    true
+  end
 
-  ## Examples
+  def create_ticket(_user, _event) do
+    Process.sleep(Enum.random(250..1000))
+  end
 
-      iex> Tickets.hello()
-      :world
+  def send_email(_user) do
+    Process.sleep(Enum.random(100..250))
+  end
 
-  """
-  def hello do
-    :world
+  @users [
+    %{id: "1", email: "foo@email.com"},
+    %{id: "2", email: "bar@email.com"},
+    %{id: "3", email: "baz@email.com"}
+  ]
+
+  def users_by_ids(ids) when is_list(ids) do
+    Enum.filter(@users, &(&1.id in ids))
   end
 end
