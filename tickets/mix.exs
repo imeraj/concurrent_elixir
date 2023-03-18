d57aaf0f94313b7b0140213922ce1f2095f26b59
Meraj <meraj.enigma@gmail.com>
2023-03-18 07:55:34 -0400

    initial commit

---
+++ tickets/mix.exs
@@ -0,0 +1,29 @@
+defmodule Tickets.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :tickets,
+      version: "0.1.0",
+      elixir: "~> 1.14",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:lager, :logger],
+      mod: {Tickets.Application, []}
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:broadway, "~> 1.0"},
+      {:broadway_rabbitmq, "~> 0.7.2"}
+    ]
+  end
+end
